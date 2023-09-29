6def7349eb1fbbfdf608ce4d9bcb630c20dff6c5
Amedeo Simone Luciano <amedeo.luciano@gmail.com>
2023-09-29 19:26:30 +0200

    fixed variable ldap zimbra

--- skell/importar_ldap.sh
+++ skell/importar_ldap.sh
@@ -16,7 +16,6 @@ NO_COLOUR="printf \e[0m" #White
 DEFAULTCOS_DN="cn=default,cn=cos,cn=zimbra"
 DEFAULTEXTERNALCOS_DN="cn=defaultExternal,cn=cos,cn=zimbra"
 SERVER_HOSTNAME=$zimbra_server_hostname
-SERVER_LDAP_HOSTNAME=$ldap_master_url
 SESSION=`date +"%d_%b_%Y-%H-%M"`
 SESSION_LOG="registro-$SESSION.log"
 
@@ -96,7 +95,7 @@ read -p "Do you want to import the ADMIN user (yes/no)?" choice
     case "$choice" in
 	  y|Y|yes|s|S|sim ) 
 	               $NORMAL_TEXT "Removing ADMIN: $ZIMBRAADMIN_DN" 
-				   ldapdelete -r -x -H ldap://$SERVER_LDAP_HOSTNAME -D $zimbra_ldap_userdn -c -w $zimbra_ldap_password $ZIMBRAADMIN_DN &>> $SESSION_LOG
+				   ldapdelete -r -x -H ldap://$SERVER_HOSTNAME -D $zimbra_ldap_userdn -c -w $zimbra_ldap_password $ZIMBRAADMIN_DN &>> $SESSION_LOG
 				   ;;
 	  n|N|no|nao ) $CHOICE_TEXT "The admin user will not be imported. Use the NEW installation password";;
 	  * ) test_importadmin ;;
@@ -108,18 +107,18 @@ test_importadmin #executes the test_importadmin function
 #BEGINS IMPORTATION OF SERVICE CLASSES, ACCOUNTS, ALTERNATIVE NAMES AND DISTRIBUTION LISTS
 ## REMOVE ZIMBRA'S DEFAULT COS: DEFAULT AND ZIMBRADEFAULT
 $INFO_TEXT "Removing default service classes: Default e DefaultExternal"
-ldapdelete -r -x -H ldap://$SERVER_LDAP_HOSTNAME -D $zimbra_ldap_userdn -c -w $zimbra_ldap_password $DEFAULTCOS_DN &>> $SESSION_LOG
-ldapdelete -r -x -H ldap://$SERVER_LDAP_HOSTNAME -D $zimbra_ldap_userdn -c -w $zimbra_ldap_password $DEFAULTEXTERNALCOS_DN &>> $SESSION_LOG
+ldapdelete -r -x -H ldap://$SERVER_HOSTNAME -D $zimbra_ldap_userdn -c -w $zimbra_ldap_password $DEFAULTCOS_DN &>> $SESSION_LOG
+ldapdelete -r -x -H ldap://$SERVER_HOSTNAME -D $zimbra_ldap_userdn -c -w $zimbra_ldap_password $DEFAULTEXTERNALCOS_DN &>> $SESSION_LOG
 
 ## IMPORT OF COS, ACCOUNTS, ALIAS AND DISTRIBUTION LISTS
 
 $INFO_TEXT "Importing COS"
-ldapadd -c -x -H ldap://$SERVER_LDAP_HOSTNAME -D $zimbra_ldap_userdn -w $zimbra_ldap_password -f COS.ldif &>> $SESSION_LOG
+ldapadd -c -x -H ldap://$SERVER_HOSTNAME -D $zimbra_ldap_userdn -w $zimbra_ldap_password -f COS.ldif &>> $SESSION_LOG
 $INFO_TEXT "Importing accounts"
-ldapadd -c -x -H ldap://$SERVER_LDAP_HOSTNAME -D $zimbra_ldap_userdn -w $zimbra_ldap_password -f ACCOUNTS.ldif &>> $SESSION_LOG
+ldapadd -c -x -H ldap://$SERVER_HOSTNAME -D $zimbra_ldap_userdn -w $zimbra_ldap_password -f ACCOUNTS.ldif &>> $SESSION_LOG
 $INFO_TEXT "importing alias"
-ldapadd -c -x -H ldap://$SERVER_LDAP_HOSTNAME -D $zimbra_ldap_userdn -w $zimbra_ldap_password -f ALIAS.ldif &>> $SESSION_LOG
+ldapadd -c -x -H ldap://$SERVER_HOSTNAME -D $zimbra_ldap_userdn -w $zimbra_ldap_password -f ALIAS.ldif &>> $SESSION_LOG
 $INFO_TEXT "import distribution lists"
-ldapadd -c -x -H ldap://$SERVER_LDAP_HOSTNAME -D $zimbra_ldap_userdn -w $zimbra_ldap_password -f LISTAS.ldif &>> $SESSION_LOG
+ldapadd -c -x -H ldap://$SERVER_HOSTNAME -D $zimbra_ldap_userdn -w $zimbra_ldap_password -f LISTAS.ldif &>> $SESSION_LOG
 
 #
